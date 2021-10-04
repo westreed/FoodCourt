04b15929c53bc51c6c2e7c361ea8063791b1f01c
Anixz <westreed@naver.com>
2021-10-04 18:39:04 +0900

    키보드에 의해 화면이 가리는 현상 해결

---
+++ screens/Payment.js
@@ -0,0 +1,32 @@
+import React, {useContext, useEffect} from "react";
+import {
+    SafeAreaView,
+    View,
+    Text,
+    StyleSheet,
+    TouchableOpacity,
+    Image,
+    TextInput,
+} from "react-native";
+
+import FormButton from '../components/FormButton';
+import BackArrowSvg from '../assets/icons/back-arrow-direction-down-right-left-up-svgrepo-com.svg';
+import { SIZES, COLORS, FONTS } from '../constants';
+import {AuthContext} from '../navigation/AuthProvider';
+
+const Payment = ({ navigation }) => {
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text></Text>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: COLORS.white2
+    },
+})
+
+export default Payment;
